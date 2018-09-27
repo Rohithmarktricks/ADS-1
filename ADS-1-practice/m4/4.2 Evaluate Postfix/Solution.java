87ae96f46a971d3c959a2102dcafa74bc52df206
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-09-27 20:39:02 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1-practice/m4/4.2 Evaluate Postfix/Solution.java
+++ ADS-1-practice/m4/4.2 Evaluate Postfix/Solution.java
@@ -1,28 +1,37 @@
 import java.util.Scanner;
-import java.util.Arrays;
+/**
+ * Class for solution.
+ * @author : Rohith.
+ */
 public class Solution {
-	Solution() {
-
-	}
-
-
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		MyStack<Integer> stack = new MyStack<>();
-		String[] str = scan.nextLine().split(" ");
-		for (int i = 0; i < str.length; i++) {
-			if ((!str[i].equals("*")) && (!str[i].equals("+")) ) {
-				stack.push(Integer.parseInt(str[i]));
-			} else {
-				int num1 = stack.pop();
-				int num2 = stack.pop();
-				if (str[i].equals("*")) {
-					stack.push(num1 * num2);
-				} else {
-					stack.push(num1 + num2);
-				}
-			}
-		}
-		System.out.println(stack.pop());
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused.
+    }
+    /**
+     * Main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        MyStack<Integer> stack = new MyStack<>();
+        String[] str = scan.nextLine().split(" ");
+        for (int i = 0; i < str.length; i++) {
+            if ((!str[i].equals("*")) && (!str[i].equals("+"))) {
+                stack.push(Integer.parseInt(str[i]));
+            } else {
+                int num1 = stack.pop();
+                int num2 = stack.pop();
+                if (str[i].equals("*")) {
+                    stack.push(num1 * num2);
+                } else {
+                    stack.push(num1 + num2);
+                }
+            }
+        }
+        System.out.println(stack.pop());
+    }
 }
