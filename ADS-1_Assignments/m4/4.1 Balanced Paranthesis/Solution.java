b49e4fbcfa0c6cf1834b89c404e0137f72efe176
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-09-27 22:17:47 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1_Assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS-1_Assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -3,56 +3,56 @@ import java.util.Scanner;
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused Constructor.
-	}
-	/**
-	 * Checks if Parenthesis is balanced.
-	 *
-	 * @param      s     String.
-	 *
-	 * @return     Trueif balanced, Otherwise False.
-	 */
-	public static boolean balancedParenthensies(String s) {
-		MyStack<Character> stack = new MyStack<>();
-		for (int i = 0; i < s.length(); i++) {
-			Character c = s.charAt(i);
-			if (c == '[' || c == '(' || c == '{' ) {
-				stack.push(c);
-			} else if (c == ']') {
-				if (stack.isEmpty() || stack.pop() != '[') {
-					return false;
-				}
-			} else if (c == ')') {
-				if (stack.isEmpty() || stack.pop() != '(') {
-					return false;
-				}
-			} else if (c == '}') {
-				if (stack.isEmpty() || stack.pop() != '{') {
-					return false;
-				}
-			}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused Constructor.
+    }
+    /**
+     * Checks if Parenthesis is balanced.
+     *
+     * @param      s     String.
+     *
+     * @return     Trueif balanced, Otherwise False.
+     */
+    public static boolean balancedParenthensies(final String str) {
+        MyStack<Character> stack = new MyStack<>();
+        for (int i = 0; i < str.length(); i++) {
+            Character c = str.charAt(i);
+            if (c == '[' || c == '(' || c == '{') {
+                stack.push(c);
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            } else if (c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            } else if (c == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            }
 
-		}
-		return stack.isEmpty();
-	}
-	/**
-	 * Main method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int num = scan.nextInt();
-		while (scan.hasNext()) {
-			if (balancedParenthensies(scan.next())) {
-				System.out.println("YES");
-			} else {
-				System.out.println("NO");
-			}
-		}
-	}
+        }
+        return stack.isEmpty();
+    }
+    /**
+     * Main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        while (scan.hasNext()) {
+            if (balancedParenthensies(scan.next())) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
 }
