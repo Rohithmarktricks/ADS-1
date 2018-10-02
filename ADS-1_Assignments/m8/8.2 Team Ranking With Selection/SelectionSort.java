b4f24f2f0e10f612dc01b74d2e9d8bf45a51ce3a
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-10-02 22:50:15 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- ADS-1_Assignments/m8/8.2 Team Ranking With Selection/SelectionSort.java
+++ ADS-1_Assignments/m8/8.2 Team Ranking With Selection/SelectionSort.java
@@ -1,5 +1,12 @@
+/**
+ * Class for selection sort.
+ */
 class SelectionSort {
-
+	/**
+	 * Sorting
+	 *
+	 * @param      a     List of type Team.
+	 */
 	public void sort(List<Team> a) {
 		int n = a.size();
 		for (int i = 0; i < n; i++) {
@@ -10,12 +17,25 @@ class SelectionSort {
 			swap(a, i, min);
 		}
 	}
-
-
+	/**
+	 * Checks Greater.
+	 *
+	 * @param      a     type Team
+	 * @param      b     type Team
+	 *
+	 * @return     boolean value.
+	 */
 	private  boolean greater(Team a, Team b) {
 		return a.compareTo(b) > 0;
 	}
 
+	/**
+	 * Swap the values.
+	 *
+	 * @param      a     list of type Team
+	 * @param      i     int position.
+	 * @param      j     int position.
+	 */
 	private  void swap(List<Team> a, int i, int j) {
 		a.replace(i, j);
 	}
