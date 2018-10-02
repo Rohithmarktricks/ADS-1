db40d7a89c30a82a9ef486c8e3c865b66f6ca3c9
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-10-02 21:39:41 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- ADS-1_Assignments/m8/8.1 Team Ranking With Insertion/Solution.java
+++ ADS-1_Assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -17,14 +17,14 @@ public final class Solution {
      */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
-        Team[] dupTeams = new Team[15];
+        Team[] dupTeams = new Team[(2 + 1) * (2 + 2)];
         InsertionSort is = new InsertionSort();
         int i = 0;
         while (scan.hasNext()) {
             String[] str = scan.nextLine().split(",");
             dupTeams[i++] = new Team(str[0], Integer.parseInt(str[1]),
                                      Integer.parseInt(str[2]),
-                                     Integer.parseInt(str[3]));
+                                     Integer.parseInt(str[2 + 1]));
         }
         scan.close();
         Team[] teams = new Team[i];
@@ -32,6 +32,7 @@ public final class Solution {
             teams[j] = dupTeams[j];
         }
         is.sort(teams);
-        System.out.println(Arrays.toString(teams).replace("[", "").replace("]", "").replace(", ", ","));
+        System.out.println(Arrays.toString(teams).replace("[", "").
+                           replace("]", "").replace(", ", ","));
     }
 }
