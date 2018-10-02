1a868d4ef99a094a568f0c3c5c63c83e02a16a91
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-10-02 22:04:53 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- ADS-1_Assignments/m8/8.1 Team Ranking With Insertion/InsertionSort.java
+++ ADS-1_Assignments/m8/8.1 Team Ranking With Insertion/InsertionSort.java
@@ -1,6 +1,6 @@
 class InsertionSort {
 
-	static void sort(List<Team> a) {
+	public void sort(List<Team> a) {
 		int n = a.size();
 		for (int i = 0; i < n; i++) {
 			for (int j = i; j > 0 && greater(a.get(j), a.get(j - 1)); j--) {
@@ -9,11 +9,11 @@ class InsertionSort {
 		}
 	}
 
-	private static boolean greater(Team a, Team b) {
+	private  boolean greater(Team a, Team b) {
 		return a.compareTo(b) > 0;
 	}
 
-	private static void swap(List<Team> a, int i, int j) {
-		a.replace(i,j);
+	private  void swap(List<Team> a, int i, int j) {
+		a.replace(i, j);
 	}
 }
--- ADS-1_Assignments/m8/8.1 Team Ranking With Insertion/Solution.java
+++ ADS-1_Assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -17,20 +17,14 @@ public final class Solution {
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         List<Team> list = new List<Team>();
-        //Team[] dupTeams = new Team[(2 + 1) * (2 + 2)];
         InsertionSort is = new InsertionSort();
-        int i = 0;
         while (scan.hasNext()) {
             String[] str = scan.nextLine().split(",");
             list.add(new Team(str[0], Integer.parseInt(str[1]),
-                                     Integer.parseInt(str[2]),
-                                     Integer.parseInt(str[2 + 1])));
+                              Integer.parseInt(str[2]),
+                              Integer.parseInt(str[2 + 1])));
         }
         scan.close();
-        // Team[] teams = new Team[i];
-        // for (int j = 0; j < i; j++) {
-        //     teams[j] = dupTeams[j];
-        // }
         is.sort(list);
         System.out.println(list.toString().replace("[", "").
                            replace("]", "").replace(", ", ","));
