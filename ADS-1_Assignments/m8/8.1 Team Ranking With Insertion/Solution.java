695e2d7baaa21ad22cb103b34d06a6a2e8231206
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-10-02 21:36:10 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- ADS-1_Assignments/m8/8.1 Team Ranking With Insertion/Solution.java
+++ ADS-1_Assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -1,23 +1,37 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+ * Class for solution.
+ */
 public class Solution {
-
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		Team[] dupTeams = new Team[15];
-		InsertionSort is = new InsertionSort();
-		int i = 0;
-		while (scan.hasNext()) {
-			String[] str = scan.nextLine().split(",");
-			dupTeams[i++] = new Team(str[0], Integer.parseInt(str[1]),
-			                    Integer.parseInt(str[2]),
-			                    Integer.parseInt(str[3]));
-		}
-		scan.close();
-		Team[] teams = new Team[i];
-		for (int j = 0; j < i; j++) {
-			teams[j] = dupTeams[j];
-		}
-		is.sort(teams);
-		System.out.println(Arrays.toString(teams).replace("[", "").replace("]", "").replace(", ", ","));
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution(){
+        //unused.
+    }
+    /**
+     * Main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Team[] dupTeams = new Team[15];
+        InsertionSort is = new InsertionSort();
+        int i = 0;
+        while (scan.hasNext()) {
+            String[] str = scan.nextLine().split(",");
+            dupTeams[i++] = new Team(str[0], Integer.parseInt(str[1]),
+                                Integer.parseInt(str[2]),
+                                Integer.parseInt(str[3]));
+        }
+        scan.close();
+        Team[] teams = new Team[i];
+        for (int j = 0; j < i; j++) {
+            teams[j] = dupTeams[j];
+        }
+        is.sort(teams);
+        System.out.println(Arrays.toString(teams).replace("[", "").replace("]", "").replace(", ", ","));
+    }
 }
