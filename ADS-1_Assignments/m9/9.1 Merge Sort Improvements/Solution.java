585286e2044396303e340473aeb26da5b78ed36b
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-10-03 21:17:27 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1_Assignments/m9/9.1 Merge Sort Improvements/Solution.java
+++ ADS-1_Assignments/m9/9.1 Merge Sort Improvements/Solution.java
@@ -1,30 +1,36 @@
 import java.util.Arrays;
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 public class Solution {
-	Solution() {
-		//unused Constructor.
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused Constructor.
+    }
 
-	/**
-	 * Main method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		// StringBuffer sb = new StringBuffer();
-		while (scan.hasNext()) {
-			// sb.append(scan.nextLine()+":")
-			String b = scan.nextLine();
-			// System.out.println(b);
+    /**
+     * Main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // StringBuffer sb = new StringBuffer();
+        while (scan.hasNext()) {
+            // sb.append(scan.nextLine()+":")
+            String b = scan.nextLine();
+            // System.out.println(b);
 
-			String[] a = b.split(",");
-			// System.out.println(Arrays.toString(a));
-			MergedSortImproved.sort(a);
-			System.out.println(Arrays.toString(a));
-			System.out.println();
+            String[] a = b.split(",");
+            // System.out.println(Arrays.toString(a));
+            MergedSortImproved.sort(a);
+            System.out.println(Arrays.toString(a));
+            System.out.println();
 
-		}
+        }
 
-	}
+    }
 }
