d1c4d9c4e70ca872abd3e5d5412d22e96f616851
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-10-06 13:12:51 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1_Assignments/m12/Assignment-1/Solution.java
+++ ADS-1_Assignments/m12/Assignment-1/Solution.java
@@ -1,37 +1,49 @@
-import java.util.*;
+import java.util.Scanner;
 // import java.util.Scanner;
+
+/**
+ * Class for solution.
+ */
 public class Solution {
-	Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		InsertionSort is = new InsertionSort();
-		int qualified = scan.nextInt();
-		int vacancies = scan.nextInt();
-		int unreserved = scan.nextInt();
-		int nbc = scan.nextInt();
-		int nsc = scan.nextInt();
-		int nst = scan.nextInt();
-		Student[] student = new Student[qualified];
+    }
+    /**
+     * Main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        InsertionSort is = new InsertionSort();
+        int qualified = scan.nextInt();
+        int vacancies = scan.nextInt();
+        int unreserved = scan.nextInt();
+        int nbc = scan.nextInt();
+        int nsc = scan.nextInt();
+        int nst = scan.nextInt();
+        Student[] student = new Student[qualified];
 
-		for (int i = 0; i < qualified; i++) {
-			String[] str = scan.next().split(",");
-			String[] str2 = str[1].split("-");
-			student[i] = new Student(str[0], Integer.parseInt(str2[0]), Integer.parseInt(str2[1]), Integer.parseInt(str2[2]),
-			                         Integer.parseInt(str[2]), Integer.parseInt(str[3]),
-			                         Integer.parseInt(str[4]), Integer.parseInt(str[5]), str[6]);
-		}
-		scan.close();
-		is.sort(student);
+        for (int i = 0; i < qualified; i++) {
+            String[] str = scan.next().split(",");
+            String[] str2 = str[1].split("-");
+            student[i] = new Student(str[0], Integer.parseInt(str2[0]), Integer.parseInt(str2[1]), Integer.parseInt(str2[2]),
+                                     Integer.parseInt(str[2]), Integer.parseInt(str[2 + 1]),
+                                     Integer.parseInt(str[2 + 2]), Integer.parseInt(str[2 + 2 + 1]), str[2 + 2 + 2]);
+        }
+        scan.close();
+        is.sort(student);
 
-		for (int i = 0; i < qualified; i++) {
-			System.out.println(student[i].name + "," + student[i].totalMarks + "," + student[i].category);
-		}
-		System.out.println();
-		for (int j = 0; j < unreserved; j++) {
-			System.out.println(student[j].name + "," + student[j].totalMarks + "," + student[j].category);
-		}
+        for (int i = 0; i < qualified; i++) {
+            System.out.println(student[i].name + "," + student[i].totalMarks + "," + student[i].category);
+        }
+        System.out.println();
+        for (int j = 0; j < unreserved; j++) {
+            System.out.println(student[j].name + "," + student[j].totalMarks + "," + student[j].category);
+        }
 
-	}
+    }
 }
