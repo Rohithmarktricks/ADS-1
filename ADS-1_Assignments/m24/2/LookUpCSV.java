7571d030c4df061382d6311faf74d1fed2e0a357
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-10-27 13:15:43 +0530

    "m24_a2_1: 3/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1_Assignments/m24/2/LookUpCSV.java
@@ -0,0 +1,19 @@
+public class LookUpCSV {
+	private final int sroll;
+	private final String sname;
+
+	LookUpCSV(int sroll1, String sname1) {
+		this.sroll = sroll1;
+		this.sname = sname1;
+	}
+
+	public int getRoll() {
+		return this.sroll;
+	}
+
+	public String getName() {
+		return this.sname;
+	}
+
+
+}
