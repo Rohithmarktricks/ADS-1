5e1e55a189f94f875c906410b98c8220e6f0c361
Rohithmarktricks <rohithbanka05@msitprogram.net>
2018-10-28 18:42:15 +0530

    "ads1_m14_a2: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-1_Assignments/m14/14.2 student reservation system/Solution.java
+++ ADS-1_Assignments/m14/14.2 student reservation system/Solution.java
@@ -1,56 +1,39 @@
-/**
- * import Scanner class
- */
 import java.util.Scanner;
 /**
- * @author : Rohith.
+ * Class for solution.
  */
 final class Solution {
     /**
-     * Solution class for Insertion sort program.
-     *
+     * Constructs the object.
      */
     private Solution() {
-        /**
-         * { item_description }.
-         */
+        //function.
     }
     /**
-     * Main function to take inputs.
+     * main.
      *
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
-    /**main method converts the given input data into
-    array objects and pass as parameters to the sorting class.**/
         Scanner sc = new Scanner(System.in);
-        int studentsQualified = sc.nextInt();
-        int numOfvacancies = sc.nextInt();
-        int unreserved_vacancies = sc.nextInt();
-        int bc_vacancies = sc.nextInt();
-        int sch_vacancies = sc.nextInt();
-        int sct_vacancies = sc.nextInt();
-        StringBuffer sb = new StringBuffer();
-        while (sc.hasNext()) {
-            sb.append(sc.next() + "::");
-        }
-        String[] lines = sb.toString().split("::");
+        int n = Integer.parseInt(sc.nextLine());
+        int o = Integer.parseInt(sc.nextLine());
+        int p = Integer.parseInt(sc.nextLine());
+        int q = Integer.parseInt(sc.nextLine());
+        int r = Integer.parseInt(sc.nextLine());
+        int s = Integer.parseInt(sc.nextLine());
+        Studentdetails[] list = new Studentdetails[n];
+        int temp = n;
         int i = 0;
-        Student[] students = new Student[lines.length];
-        for (String line : lines) {
-            String[] tokens = line.split(",");
-            String[] date = tokens[1].split("-");
-            Student student = new Student(tokens[0], date[0], date[1], date[2],
-                Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]),
-                Integer.parseInt(tokens[4]), Integer.parseInt(tokens[5]), 
-                tokens[6]);
-            students[i++] = student;
+        while (n > 0) {
+            String[] tokens = sc.next().split(",");
+            list[i++] = new Studentdetails(tokens[0], tokens[1], Integer.parseInt(tokens[2]),
+            Integer.parseInt(tokens[3]), Integer.parseInt(tokens[4]),
+            Integer.parseInt(tokens[5]), tokens[6]);
+            n--;
         }
-        Heap.sort(students);
-        String output = "";
-        for (Student student : students) {
-            System.out.println(student);
-        }
-        System.out.println();
+        Heapsort h = new Heapsort(o, p, q, r, s, list, i);
+        list = h.sort();
+        h.board(temp);
     }
 }
